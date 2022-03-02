c3dfeff96c4a7ac98df568c059274b0f70c4bd28
user <user@users-MacBook-Pro.local>
2022-03-02 21:22:49 +0000

    Finished mvp

---
+++ src/compare.py
@@ -0,0 +1,7 @@
+def compare(number_1, number_2):
+    if number_1 > number_2:
+        return f"{number_1} is greater than {number_2}"
+    elif number_2 > number_1:
+        return f"{number_2} is greater than {number_1}"
+    elif number_1 == number_2:
+        return f"{number_2} is equal to  {number_1}"
---
+++ tests/compare_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.compare import compare
+
+class TestCompare(unittest.TestCase):
+
+    def test_compare_3_1_returns_3_is_greater_than_1(self):
+        self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_two_same_numbers(self):
+        self.assertEqual("3 is equal to 3", compare(3, 3))
